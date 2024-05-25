26aacfdea0f173335fd92690d83bd99d9a8cf96e
Matias Ruiz <mruiz@icapitalnetwork.com>
2024-05-25 15:46:23 -0400

    Install angular/material, and redefine home and login pages

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  usuario: string="";
+  password: string="";
+
   constructor() { }
 
   ngOnInit() {
